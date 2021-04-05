bc797ec60e4823904026f4dd7ab234c4cce6200b
joaopfds <joao.sousa2811@gmail.com>
2021-04-04 21:20:09 -0300

    inicializando

--- leriando/src/main/java/com/estdois/leriando/controller/PostController.java
+++ leriando/src/main/java/com/estdois/leriando/controller/PostController.java
@@ -1,9 +1,8 @@
 package com.estdois.leriando.controller;
 
-
-import com.estdois.leriando.com.estdois.leriando.persistence.PostRepository;
 import com.estdois.leriando.entity.Post;
 import com.estdois.leriando.entity.Usuario;
+import com.estdois.leriando.persistence.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
