bdc9314a1842ed23858aaaa18bb13af3f75b54ba
joaopfds <joao.sousa2811@gmail.com>
2021-04-05 20:54:06 -0300

    atualizando

---
+++ leriando/src/main/java/com/estdois/leriando/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.estdois.leriando.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String telaPincipal(){
+        return "home";
+    }
+}
