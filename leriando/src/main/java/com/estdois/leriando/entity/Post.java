b663f1c5accba637eb5bd7f1ff8de2f7c36ffa92
joaopfds <joao.sousa2811@gmail.com>
2021-04-04 21:54:40 -0300

    inicializando

--- leriando/src/main/java/com/estdois/leriando/entity/Post.java
+++ leriando/src/main/java/com/estdois/leriando/entity/Post.java
@@ -3,9 +3,11 @@ package com.estdois.leriando.entity;
 import com.sun.istack.NotNull;
 import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Entity
 public class Post {
     @Id
     @GeneratedValue(generator = "increment")
