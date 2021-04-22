da66e5883791bf4a5aa15d9ceaeadfb2eedc3ad1
joaopfds <joao.sousa2811@gmail.com>
2021-04-21 22:04:39 -0300

    atualizando

--- leriando/src/main/java/com/estdois/leriando/persistence/ComentarioRepository.java
+++ leriando/src/main/java/com/estdois/leriando/persistence/ComentarioRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface ComentarioRepository extends CrudRepository<Comentario, Long> {
-    @Query("SELECT c FROM Comentario c join Post p on p.id = c.post.id")
+    @Query("SELECT c FROM Comentario c join Post p on (p.id = c.post.id) where p.id = ?1")
     List<Comentario> findComentarios(long id);
 
 }
